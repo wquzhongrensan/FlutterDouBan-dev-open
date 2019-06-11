6ea2e7a37d1e9f5692d3e81e6d7ea7667547c4df
CMIC <956023769@qq.com>
2019-06-11 17:25:55 +0800

    Initial commit

---
+++ lib/http/mock_request.dart
@@ -0,0 +1,36 @@
+import 'package:douban_app/http/API.dart';
+import 'dart:async';
+import 'package:flutter/services.dart' show rootBundle;
+import 'dart:convert';
+
+///模拟数据
+class MockRequest {
+
+  Future<dynamic> get(String action, {Map params}) async {
+    return MockRequest.mock(action: getJsonName(action), params: params);
+  }
+
+  static Future<dynamic> post({String action, Map params}) async {
+    return MockRequest.mock(action: action, params: params);
+  }
+
+  // 直接从本地获取某个json文件 然后转成dictionary
+//   字符串的拼接 'yyy$xxx.json'
+  static Future<dynamic> mock({String action, Map params}) async {
+    var responseStr = await rootBundle.loadString('mock/$action.json');
+    var responseJson = json.decode(responseStr);
+    return responseJson;
+  }
+
+  Map<String, String> map = {
+    API.IN_THEATERS: 'in_theaters',
+    API.COMING_SOON: 'coming_soon',
+    API.TOP_250: 'top250',
+    API.WEEKLY: 'weekly',
+    API.REIVIEWS: 'reviews',
+  };
+
+  getJsonName(String action) {
+    return map[action];
+  }
+}
